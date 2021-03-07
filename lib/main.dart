4cf2525f0b8cb7f25da34f3fe378fd74601fe3b4
yuri.pereira <yuri.pereira@ambevtech.com.br>
2021-03-07 18:05:19 -0300

    Projeto básico, para entender o funcionamento dos Widgets no Flutter. conseguir entender bem como é.

--- lib/main.dart
+++ lib/main.dart
@@ -34,15 +34,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,56 +41,69 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _texto = "Pode Entrar!";
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _atualizaPessoasRestaurant(int valor) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter--;
+      _counter += valor;
+      _atualizaTextoTela();
     });
+  }
+
+  void _atualizaTextoTela(){
+    if(_counter < 0){
+      _texto = "Mundo invertido?!";
+    }else if(_counter <= 10){
+      _texto = "Pode Entrar";
+    }else {
+      _texto = "Restaurante Cheio!";
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return Stack(
+      children: [
+        Image.asset("images/restaurant.jpg", fit: BoxFit.cover, height: 1000.0),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("Pessoas: $_counter",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text("+1",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                  onPressed: () {
+                    _atualizaPessoasRestaurant(1);
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: FlatButton(
+                  child: Text("-1",
+                      style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                  onPressed: () {
+                    _atualizaPessoasRestaurant(-1);
+                  },
+                ),
+              )
+            ],
+          ),
+          Text(
+            _texto,
+            style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0),
+          )
+        ]),
+      ],
     );
   }
 }
